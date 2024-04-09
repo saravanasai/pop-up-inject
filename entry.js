2d140e02a143be82691227026fa201eba04d94a7
INRDEVTHIYAGLT1\sthiyagarajan <sthiyagarajan@uplandsoftware.com>
2024-04-09 17:58:47 +0530

    Changed windth

--- entry.js
+++ entry.js
@@ -17,8 +17,8 @@ class UCM {
       this.ifrm.setAttribute("allow", "fullscreen");
       this.ifrm.setAttribute("allowtransparency", true);
       this.ifrm.setAttribute("scrolling", "no");
-      this.ifrm.style.width = "300px";
-      this.ifrm.style.height = "300px";
+      this.ifrm.style.width = "310px";
+      this.ifrm.style.height = "310px";
       this.ifrm.style.borderWidth = "0px";
       container.appendChild(this.ifrm);
     }
