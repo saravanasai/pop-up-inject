ca0458bb7b4769c52041d2b2f8bc06e8347b7d8d
INRDEVTHIYAGLT1\sthiyagarajan <sthiyagarajan@uplandsoftware.com>
2024-04-16 16:09:21 +0530

    added a staic class to handle hide & show

--- entry.js
+++ entry.js
@@ -2,6 +2,9 @@ class UCM {
   constructor(config) {
     this.configuration = config;
     this.ifrm = document.createElement("iframe");
+    this.container = document.createElement("div");
+    this.css =
+      ".app-swicther-popup{z-index: 100000000 !important;}.show {display: block;}.hide {display: none;}";
   }
 
   init() {
@@ -28,24 +31,32 @@ class UCM {
   }
 
   prepareFrame(top, left) {
+    const styleEl = document.createElement("style");
+    styleEl.append(this.css);
     this.ifrm.setAttribute("src", "http://app-switcher.test/");
     this.ifrm.setAttribute("allowfullscreen", true);
     this.ifrm.setAttribute("allow", "fullscreen");
     this.ifrm.setAttribute("allowtransparency", true);
     this.ifrm.setAttribute("scrolling", "no");
-    this.ifrm.style.width = "419px";
-    this.ifrm.style.height = "475px";
+    this.ifrm.style.width = "425px";
+    this.ifrm.style.height = "500px";
     this.ifrm.style.position = "absolute";
     this.ifrm.style.top = top + "px";
     this.ifrm.style.left = left + "px";
     this.ifrm.style.borderWidth = "0px";
-    document.body.appendChild(this.ifrm);
+    this.ifrm.setAttribute("class", "app-swicther-popup hide");
+    this.container.appendChild(this.ifrm);
+    document.body.appendChild(this.container);
   }
 
   show() {
+    this.ifrm.classList.remove("hide");
+    this.ifrm.classList.add("show");
     this.handleMessage("show");
   }
   hide() {
+    this.ifrm.classList.remove("show");
+    this.ifrm.classList.add("hide");
     this.handleMessage("hide");
   }
 
